7b685833308c9f28bc29807b4e71fdabe742ccfd
GuiMatheus1313 <82453465+GuiMatheus1313@users.noreply.github.com>
2021-11-03 11:31:58 -0300

    Inserindo o inicio do funcionamento do BD
    início da inserção, ainda está faltando as models, revisão etc.

--- GAME_WAVE - SISTEMA ASP_NET/Repositorio/Acoes.cs
+++ GAME_WAVE - SISTEMA ASP_NET/Repositorio/Acoes.cs
@@ -1,11 +1,20 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using GAME_WAVE___SISTEMA_ASP_NET.Repositorio;
+using MySql.Data.MySqlClient;
+using GAME_WAVE___SISTEMA_ASP_NET.Models;
+using 
 
-namespace GAME_WAVE___SISTEMA_ASP_NET.Repositorio
+namespace GAME_WAVE___SISTEMA_ASP_NET.Controllers
 {
     public class Acoes
     {
+        ConexaoSql conectar = new ConexaoSql();
+        MySqlCommand cmd = new MySqlCommand();
+        
+        /* Incompleto BD, esperando a revisão da análise */
+        
     }
 }
