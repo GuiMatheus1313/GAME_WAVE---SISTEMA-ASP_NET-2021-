7afb404f424cda46cbb541c84e7ada9844331811
GuiMatheus1313 <82453465+GuiMatheus1313@users.noreply.github.com>
2021-11-03 11:19:02 -0300

    Inserção da conexão inicial do BD do cliente
    Identificação do banco, tanto apenas para conectar ao banco e desconectar.

--- GAME_WAVE - SISTEMA ASP_NET/Repositorio/Conexaosql.cs
+++ GAME_WAVE - SISTEMA ASP_NET/Repositorio/Conexaosql.cs
@@ -7,5 +7,33 @@ namespace GAME_WAVE___SISTEMA_ASP_NET.Repositorio
 {
     public class Conexaosql
     {
+        MySqlConnection conexao = new MySqlConnection("Server=localhost;Database=BDLojaJogos_tcm;user=root;pwd=guiguiba");
+        public static string msg; 
+        
+        MySqlConnection ConectarBD()
+        {
+            try
+            {            
+                conexao.Open();
+            }
+            catch(Exception erro)
+            {
+                msg = "Ocorreu um erro ao se conectar" + erro.Message;
+            }
+            return conexao
+          }
+        
+        public MySqlConnection DesconectarBD()
+        {
+            try 
+            {
+                conexao.Close();
+            }
+            catch (Exception erro)
+            {
+                msg = "Ocorreu um erro ao se desconectar" + erro.Message;
+            }
+            return conexao;
+        }
     }
 }
