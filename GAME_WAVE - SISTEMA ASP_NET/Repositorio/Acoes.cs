1265a1b34c5335a01d845d9ccfe3014b8456c6fe
GuiMatheus1313 <82453465+GuiMatheus1313@users.noreply.github.com>
2021-11-24 11:34:14 -0300

    aq
    aq

--- GAME_WAVE - SISTEMA ASP_NET/Repositorio/Acoes.cs
+++ GAME_WAVE - SISTEMA ASP_NET/Repositorio/Acoes.cs
@@ -16,6 +16,21 @@ namespace GAME_WAVE___SISTEMA_ASP_NET.Controllers
         MySqlCommand cmd = new MySqlCommand();
 
         /* Incompleto BD, esperando a revisão da análise */
+        
+        public void CadastrarCliente(Classe_Cliente cli)
+        {
+            MySqlCommand cmd = new MySqlCommand("insert into Cliente values(@cli_cpf,@cli_nome,@cli_numend,@cli_email,@cli_tel,@fk_fidelidade_brinde_cod,@fk_cep_cep)", con.ConectarBD());
+            cmd.Parameters.Add("@cli_cpf", MySqlDbType.numeric).Value = cli.Cli_cpf;
+            cmd.Parameters.Add("@cli_nome", MySqlDbType.varchar).Value = cli.Cli_Nome;
+            cmd.Parameters.Add("@cli_numend", MySqlDbType.int).Value = cli.Cli_numEnd;
+            cmd.Parameters.Add("@cli_email", MySqlDbType.varchar).Value = cli.Cli_email;
+            cmd.Parameters.Add("@cli_tel", MySqlDbType.numeric).Value = cli.Cli_tel;
+            cmd.Parameters.Add("@flarchar).Value = cli.Cli_Nome;
+
+
+
+
+        }
 
     }
 }
