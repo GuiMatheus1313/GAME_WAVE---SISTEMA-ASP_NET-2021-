7b3a565eb8e7c4edd43613c271035fc60b125f2d
GuiMatheus1313 <82453465+GuiMatheus1313@users.noreply.github.com>
2021-11-25 11:24:17 -0300

    atualização de açoes
    inserção de insert, alt e deleta das 3 classes disponíveis

--- GAME_WAVE - SISTEMA ASP_NET/Repositorio/Acoes.cs
+++ GAME_WAVE - SISTEMA ASP_NET/Repositorio/Acoes.cs
@@ -77,21 +77,63 @@ namespace GAME_WAVE___SISTEMA_ASP_NET.Controllers
         public void AltFuncio(Classe_Funcionario funci)
         {
             MySqlCommand cmd = new MySqlCommand("update TBFuncionario set func_Nome=@func_Nome, func_Tel=@func_Tel, func_Email=@func_Email, func_DataNasc=@func_DataNasc, func_Num_End=@func_Num_End, func_Cargo=@func_Cargo, fk_Cep_cep=@fk_Cep_cep where Func_CPF=@func_Cpf", con.ConectarBD());
-            cmd.Parameters.Add("@func_Nome", MySQLDbType.varchar).Value = funci.Func_nome;
-            cmd.Parameters.Add("@func_Tel", MySQLDbType.Tel).Value = funci.Func_tel;
-            cmd.Parameters.Add("@func_Email", MySQLDbType.varchar).Value = funci.Func_email;
-            cmd.Parameters.Add("@func_DataNasc", MySQLDbType.DataNasc).Value = funci.Func_datanasc;
-            cmd.Parameters.Add("@func_Num_End", MySQLDbType.int).Value = funci.Func_num_end;
-            cmd.Parameters.Add("@func_Cargo", MySQLDbType.varchar).Value = funci.Func_cargo;
+            cmd.Parameters.Add("@func_Nome", MySqlDbType.varchar).Value = funci.Func_nome;
+            cmd.Parameters.Add("@func_Tel", MySqlDbType.Tel).Value = funci.Func_tel;
+            cmd.Parameters.Add("@func_Email", MySqlDbType.varchar).Value = funci.Func_email;
+            cmd.Parameters.Add("@func_DataNasc", MySqlDbType.DataNasc).Value = funci.Func_datanasc;
+            cmd.Parameters.Add("@func_Num_End", MySqlDbType.int).Value = funci.Func_num_end;
+            cmd.Parameters.Add("@func_Cargo", MySqlDbType.varchar).Value = funci.Func_cargo;
             cmd.Parameters.Add("@fk_Cep_cep", MySqlDbType.int).Value = ;
-            cmd.ExecuteNonqQuery();
-            cmd.Desconectar();
+            cmd.ExecuteNonQuery();
+            cmd.DesconectarBD();
         }
         
         public void AltProduto(Classe_Produto produ)
         {
-            MySqlCommand cmd = new MySqlCommand("update TBProduto set prod_Nome=@prod_Nome, prod_Tipo=@prod_Tipo, prod_Quant_Estoque=@prod_Quant_Estoque, prod_Desc=@prod_Desc, prod_AnoLanc=@prod_AnoLanc, prod_FaixaEta=@prod_FaixaEta, prod_Valor=@prod_Valor where Cod_pro=@
+            MySqlCommand cmd = new MySqlCommand("update TBProduto set prod_Nome=@prod_Nome, prod_Tipo=@prod_Tipo, prod_Quant_Estoque=@prod_Quant_Estoque, prod_Desc=@prod_Desc, prod_AnoLanc=@prod_AnoLanc, prod_FaixaEta=@prod_FaixaEta, prod_Valor=@prod_Valor where Cod_pro=@cod_pROD", con.ConectarBD());
+            cmd.Parameters.Add("@prod_Nome", MySqlDbType.varchar).Value = produ.Prod_nome;
+            cmd.Parameters.Add("@prod_Tipo", MySqlDbType.varchar).Value = produ.Prod_tipo;
+            cmd.Parameters.Add("@prod_Quant_Estoque", MySqlDbType.int).Value = produ.Prod_quant_estoque;
+            cmd.Parameters.Add("@prod_Desc", MySqlDbType.varchar).Value = produ.Prod_desc;
+            cmd.Parameters.Add("@prod_AnoLanc", MySqlDbType.varchar).Value = produ.Prod_anolanc;
+            cmd.Parameters.Add("@prod_FaixaEta", MySqlDbType.int).Value = produ.Prod_faixaeta;
+            cmd.Parameters.Add("@prod_Valor", MySqlDbType.decimal).Value = produ.Prod_valor;
+            cmd.ExecuteNonQuery;
+            cmd.DesconectarBD();
         }
+        
+        //ABAIXO ESTÁ OS MÉTODOS QUE DELETA DAS CLASSES DISPONÍVEIS
+        
+        public void DelCliente(Classe_Cliente client)
+        {
+            var deletacl = String.Format("delete from TBCliente where cli_Cpf = {0}", codcl);
+            MySqlCommand cmd = new MySqlCommand(deletacl, con.ConectarBD());
+            cmd.ExecuteReader();
+        }
+        
+        public void DelFuncionario(Classe_Funcionario funcio)
+        {
+            var deletafn = String.Format("delete from TBFuncionario where func_Cod = {0}", codfn);
+            MySqlCommand cmd = new MySqlCommand(deletafn, con.ConectarBD());
+            cmd.ExecuteReader();
+        }
+        
+        public void DelProduto(Classe_Produto produt)
+        {
+            var deletapd = String.Format("delete from TBProduto where Cod_Prod = {0}", codpd;
+            MySqlCommand cmd = new MySqlCommand(deletapd, con.ConectarBD());
+            cmd.ExecuteReader();
+        }
+        
+        //INÍCIO DAS CONSULTAS DAS CLASSES DISPONÍVEIS
+         
+        //Ínicio à bCliente
+                                         
+        public List<Classe_Cliente> ListarCliente()
+        {
+            MySqlCommand cmd = new MySqlCommand("Select*from TB
+        }
+        
 
     }
 }
