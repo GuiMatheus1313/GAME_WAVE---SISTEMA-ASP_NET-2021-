453ba0e48a29a50ba52a4a657fd0af426626d3b9
GuiMatheus1313 <82453465+GuiMatheus1313@users.noreply.github.com>
2021-12-03 10:34:14 -0300

    Adição incompleta das Annotattions

--- GAME_WAVE - SISTEMA ASP_NET/Models/Classe_Cliente.cs
+++ GAME_WAVE - SISTEMA ASP_NET/Models/Classe_Cliente.cs
@@ -15,16 +15,33 @@ namespace GAME_WAVE___SISTEMA_ASP_NET.Models
 
         public long Cli_tel { get; set; }
 
+        [Display(Name = "UF")]
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "O campo apenas contém 2 caracteres")]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
         public string Nome_uf { get; set; }
-
+        
+        [Display(Name = "Cidade")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "O campo deve conter o mínimo entre 2 a 30 caracteres")]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
         public string Nome_cidade { get; set; }
-
+        
+         [Display(Name = "Bairro")]
+        [MinLength(3, ErrorMessage = "O campo deve conter o mínimo de 3 caracteres")]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
         public string Nome_bairro { get; set; }
-
+        
+        [Display(Name = "Logradouro do Funcionário")]
+        [MaxLength(100, ErrorMessage = "O campo deve conter o máximo de 100 caracteres")]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
         public string Logradouro { get; set; }
-
+        
+        [Display(Name = "CEP do Funcionário")]
+        [RegularExpression(@"^\d{5}-\d{3}$", ErrorMessage = "O formato do CEP está incorreto")]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
         public string Cep { get; set; }
-
+        
+        [Display(Name = "Número do Longradouro")]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
         public Int16 Cli_numEnd { get; set; }
     }
 }
