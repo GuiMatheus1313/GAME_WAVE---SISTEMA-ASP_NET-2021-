f3cbe3c5299b875df0937e3570966a9fd966a95d
carlosfreire15 <82534464+carlosfreire15@users.noreply.github.com>
2021-12-06 07:53:26 -0300

    UPDATE NA CLASSE_CUPOM
    adicionando Data Annotations na Model

--- GAME_WAVE - SISTEMA ASP_NET/Models/Classe_Cupom.cs
+++ GAME_WAVE - SISTEMA ASP_NET/Models/Classe_Cupom.cs
@@ -8,8 +8,12 @@ namespace GAME_WAVE___SISTEMA_ASP_NET.Models
 {
     public class Classe_Cupom
     {
+        
         public string Cupom_cod { get; set; }
-
+        
+        [Display(Name = "Cupom de Desconto")]
+        [StringLength(2, MinimumLength = 5, ErrorMessage = "O campo deve conter o valor de desconto!")]
+        [Required(ErrorMessage = "Este campo é obrigatório!")]
         public float DescontoCupom { get; set; }
     }
 }
